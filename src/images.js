d2a81d31fa4ecfbbdc5e3d366008d1f13a55ab2c
Lucas PS <lucaspsanhudo@gmail.com>
2024-09-04 18:56:17 -0300

    Adição da pasta dist no .gitignore

--- src/images.js
+++ src/images.js
@@ -1,11 +1,9 @@
 export const IMAGES = {
-  donuts : new URL('./images/donuts.jpg', import.meta.url).href,
-  carne : new URL('./images/carne.jpg', import.meta.url).href,
-  torneiras : new URL('./images/torneiras.jpg', import.meta.url).href,
-  bebidas : new URL('./images/bebidas.jpg', import.meta.url).href,
-  doces : new URL('./images/doces.svg', import.meta.url).href,
-  drinks : new URL('./images/drinks.svg', import.meta.url).href,
-  pratos : new URL('./images/pratos.svg', import.meta.url).href,
-}
-
-
+  donuts: new URL("./images/donuts.jpg", import.meta.url).href,
+  carne: new URL("./images/carne.jpg", import.meta.url).href,
+  torneiras: new URL("./images/torneiras.jpg", import.meta.url).href,
+  bebidas: new URL("./images/bebidas.jpg", import.meta.url).href,
+  doces: new URL("./images/doces.svg", import.meta.url).href,
+  drinks: new URL("./images/drinks.svg", import.meta.url).href,
+  pratos: new URL("./images/pratos.svg", import.meta.url).href,
+};
